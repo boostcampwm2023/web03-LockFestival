b1adcb0f31be183eddba6dcdd5a8826ef4536592
박정석 <wjdtjr8649@Naver.com>
2023-11-09 16:57:01 +0900

    📦 🔧 msw 및 필수 라이브러리 설치
    - emotion, axios, recoil, dotenv  설치
    - msw 실행 후 렌더링

---
+++ frontend/src/__mocks__/server.ts
@@ -0,0 +1,4 @@
+// src/mocks/node.js
+import { setupServer } from 'msw/node';
+
+export const server = setupServer();
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import initMockAPI from '@__mocks__/index.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function deferRender() {
+  if (!import.meta.env.DEV) {
+    return;
+  }
+
+  await initMockAPI();
+}
+
+deferRender().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
