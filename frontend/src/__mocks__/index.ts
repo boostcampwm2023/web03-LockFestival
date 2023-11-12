923e4f01c993289e803e336caa980f9c682078d7
navyjeongs <wjdtjr8649@naver.com>
2023-11-12 15:01:45 +0900

    🐛 테스트 환경에서만 server import하도록 변경

--- frontend/src/__mocks__/index.ts
+++ frontend/src/__mocks__/index.ts
@@ -1,11 +1,6 @@
 const initMockAPI = async (): Promise<void> => {
-  if (typeof window === 'undefined') {
-    const { server } = await import('@__mocks__/server');
-    server.listen({ onUnhandledRequest: 'bypass' });
-  } else {
-    const { worker } = await import('@__mocks__/browser');
-    worker.start();
-  }
+  const { worker } = await import('@__mocks__/browser');
+  worker.start();
 };
 
 export default initMockAPI;
