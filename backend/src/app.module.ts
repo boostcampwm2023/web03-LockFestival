f6a00898c00bdcdf21f707351e96903ba74d61f2
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-14 12:41:50 +0900

    :wrench:  typeorm, mysql 설정 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+import { DataSource } from 'typeorm';
+import { TypeormConfig } from './typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeormConfig, // TODO: typeorm 설정한 클래스
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return new DataSource(options).initialize();
+      },
     }),
   ],
   controllers: [AppController],
