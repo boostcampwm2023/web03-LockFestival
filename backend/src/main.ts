d6c8cdaf4768172b590b32a19147c8ce09881fa7
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-15 18:01:28 +0900

    :wrench:  main.ts에 ValidationPipe 글로벌 설정 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
   if (module.hot) {
     module.hot.accept();
