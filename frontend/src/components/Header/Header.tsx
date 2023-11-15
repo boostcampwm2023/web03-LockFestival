ab0a1de3d65987dd439526704f8ab270a8556900
navyjeongs <wjdtjr8649@naver.com>
2023-11-15 18:11:54 +0900

    💄 margin, padding 값 변경

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -39,14 +39,14 @@ const Header = () => {
 };
 
 const HeaderContainer = styled.header([
-  tw`font-dnfbit my-0 mx-auto text-white`,
-  tw`desktop:(w-[102.4rem] h-[6rem] text-[1.6rem])`,
-  tw`mobile:(w-full text-[1.4rem])`,
+  tw`font-dnfbit mx-auto text-white`,
+  tw`desktop:(w-[102.4rem] h-[6rem] text-l)`,
+  tw`mobile:(w-full text-m)`,
 ]);
 
 const HeaderInnerContainer = styled.div([
-  tw`desktop:(mx-[1.6rem] h-[6rem])`,
-  tw`mobile:(mx-[0.8rem] h-[4rem])`,
+  tw`desktop:(mx-4 h-[6rem])`,
+  tw`mobile:(mx-2 h-[4rem])`,
   css`
     display: flex;
     align-items: center;
@@ -63,7 +63,7 @@ const HeaderLogo = styled.img([
 
 const NavContainer = styled.ul([
   tw`font-dnfbit border border-white border-solid`,
-  tw`desktop:(w-[50rem] h-[3.3rem] mx-[1.6rem] text-[1.6rem]) rounded-[4rem]`,
+  tw`desktop:(w-[50rem] h-[3.3rem] mx-4 rounded-[4rem])`,
   tw`mobile:(hidden)`,
   css`
     display: grid;
@@ -76,7 +76,7 @@ const NavContainer = styled.ul([
 const NavItem = styled.li<{ isSelected: boolean }>(({ isSelected }) => [
   isSelected ? tw`bg-gray border border-white border-solid` : tw`bg-gray-light`,
   tw`
-    h-full px-[2rem]
+    h-full px-5
   `,
   tw`desktop:(rounded-[4rem])`,
   css`
