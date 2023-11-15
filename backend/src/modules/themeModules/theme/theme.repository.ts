868714671170a98f30031a0b732fbd2b8aa2dc41
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-15 18:10:57 +0900

    :sparkles: GET /themes/location API 개발

--- backend/src/modules/themeModules/theme/theme.repository.ts
+++ backend/src/modules/themeModules/theme/theme.repository.ts
@@ -2,6 +2,10 @@ import { Repository, DataSource } from 'typeorm';
 import { Theme } from '@theme/entities/theme.entity';
 import { Injectable } from '@nestjs/common';
 import { ThemeResponseDto } from '@theme/dtos/theme.response.dto';
+import { Branch } from '../branch/entities/branch.entity';
+import { ThemeLocationDto } from './dtos/theme.location.dto';
+
+const KM = 1000;
 
 @Injectable()
 export class ThemeRepository extends Repository<Theme> {
@@ -20,5 +24,26 @@ export class ThemeRepository extends Repository<Theme> {
     return themes.map(({ posterImageUrl, name, id }): ThemeResponseDto => {
       return new ThemeResponseDto({ posterImageUrl, name, id });
     });
+  }
+
+  async getThemesByBoundary({ x, y, boundary, count }): Promise<ThemeResponseDto[]> {
+    const themes: ThemeResponseDto[] = await this.dataSource
+      .createQueryBuilder()
+      .select([
+        'theme.id as themeId',
+        'theme.name as name',
+        'theme.poster_image_url as posterImageUrl',
+      ])
+      .from(Theme, 'theme')
+      .innerJoin('theme.branch', 'branch')
+      .where('ST_Distance_Sphere(point(branch.y, branch.x), point(:y, :x)) <= :boundary', {
+        x,
+        y,
+        boundary: boundary * KM,
+      })
+      .limit(count)
+      .getRawMany();
+
+    return themes;
   }
 }
