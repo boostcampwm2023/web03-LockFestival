027a2945a26f30be2e9227f70b9ac1ae4137f222
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-18 02:50:34 +0900

    :sparkles: 전체 장르의 id, name을 반환하는 API 개발
    - GET /themes/genres
    
      - request : 없음
    
      - response : GenreDto[]

--- backend/src/modules/themeModules/theme/theme.module.ts
+++ backend/src/modules/themeModules/theme/theme.module.ts
@@ -7,10 +7,11 @@ import { ThemeController } from '@theme/theme.controller';
 import { ThemeService } from '@theme/theme.service';
 import { ThemeRepository } from '@theme/theme.repository';
 import { GenreRepository } from '@theme/genre.repository';
+import { GenreService } from '@theme/genre.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Theme]), BrandModule, BranchModule],
   controllers: [ThemeController],
-  providers: [ThemeService, ThemeRepository, GenreRepository],
+  providers: [ThemeService, ThemeRepository, GenreRepository, GenreService],
 })
 export class ThemeModule {}
