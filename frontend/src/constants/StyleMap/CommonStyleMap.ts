95e384b4d8a9c780eeaca0c7318df808e796439c
선예 <kss02281@gmail.com>
2023-11-18 00:01:27 +0900

    💄 CommonStyleMap 수정

--- frontend/src/constants/StyleMap/CommonStyleMap.ts
+++ frontend/src/constants/StyleMap/CommonStyleMap.ts
@@ -1,27 +1,38 @@
 import tw, { TwStyle } from 'twin.macro';
 
 const fontStyleMap: Record<string, TwStyle> = {
-  dnfbit: tw`font-dnfbit`,
-  jua: tw`font-jua`,
-  gsans: tw`font-gsans`,
-  default: tw`font-gsans`,
+  pretendard: tw`font-pretendard`,
+  maplestory: tw`font-maplestory`,
+  default: tw`font-pretendard`,
 };
 
 const sizeStyleMap: Record<string, TwStyle[]> = {
-  l: [
-    tw`mobile:(text-m px-2.5 py-1.5 h-[3.2rem]) desktop:(text-l px-3 py-2 h-[3.6rem])`,
+  'l': [
+    tw`mobile:(text-m px-2.5 h-[3.2rem]) desktop:(text-l px-3 h-[3.6rem])`,
     { svg: tw`mobile:(w-[1.8rem] h-[1.8rem]) desktop:(w-[2rem] h-[2rem])` },
   ],
-  m: [
-    tw`mobile:(text-s px-2 py-1 h-[2.8rem]) desktop:(text-m px-2.5 py-1.5) h-[3.2rem]`,
+  'l-bold': [
+    tw`mobile:(text-m-bold px-2.5 h-[3.2rem]) desktop:(text-l-bold px-3 h-[3.6rem])`,
+    { svg: tw`mobile:(w-[1.8rem] h-[1.8rem]) desktop:(w-[2rem] h-[2rem])` },
+  ],
+  'm': [
+    tw`mobile:(text-s px-2 h-[2.8rem]) desktop:(text-m px-2.5) h-[3.2rem]`,
     { svg: tw`mobile:(w-[1.5rem] h-[1.5rem]) desktop:(w-[1.8rem] h-[1.8rem])` },
   ],
-  s: [
-    tw`mobile:(text-xs px-1.5 py-0.5 h-[2.4rem]) desktop:(text-s px-2 py-1 h-[2.8rem])`,
+  'm-bold': [
+    tw`mobile:(text-s-bold px-2 h-[2.8rem]) desktop:(text-m-bold px-2.5) h-[3.2rem]`,
+    { svg: tw`mobile:(w-[1.5rem] h-[1.5rem]) desktop:(w-[1.8rem] h-[1.8rem])` },
+  ],
+  's': [
+    tw`mobile:(text-xs px-1.5 h-[2.4rem]) desktop:(text-s px-2 h-[2.8rem])`,
     { svg: tw`mobile:(w-[1.2rem] h-[1.2rem]) desktop:(w-[1.5rem] h-[1.5rem])` },
   ],
-  default: [
-    tw`mobile:(text-s px-2 py-1 h-[2.8rem]) desktop:(text-m px-2.5 py-1.5 h-[3.2rem])`,
+  's-bold': [
+    tw`mobile:(text-xs-bold px-1.5 h-[2.4rem]) desktop:(text-s-bold px-2 h-[2.8rem])`,
+    { svg: tw`mobile:(w-[1.2rem] h-[1.2rem]) desktop:(w-[1.5rem] h-[1.5rem])` },
+  ],
+  'default': [
+    tw`mobile:(text-s px-2 h-[2.8rem]) desktop:(text-m px-2.5 h-[3.2rem])`,
     { svg: tw`mobile:(w-[1.5rem] h-[1.5rem]) desktop:(w-[1.8rem] h-[1.8rem])` },
   ],
 };
