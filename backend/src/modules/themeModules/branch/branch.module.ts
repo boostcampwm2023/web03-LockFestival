2dd87ee9797e54758dcefaf2ff796d45820cc0e3
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-17 12:31:19 +0900

    :sparkles: branchId에 해당하는 지점이 없으면 `404 NOT FOUND` 에러 반환

--- backend/src/modules/themeModules/branch/branch.module.ts
+++ backend/src/modules/themeModules/branch/branch.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BranchController } from './branch.controller';
 import { BranchService } from './branch.service';
 import { ThemeRepository } from '../theme/theme.repository';
+import { BranchRepository } from './branch.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Branch])],
   controllers: [BranchController],
-  providers: [BranchService, ThemeRepository],
+  providers: [BranchService, ThemeRepository, BranchRepository],
 })
 export class BranchModule {}
