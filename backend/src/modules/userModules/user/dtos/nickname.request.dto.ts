3d868fe93b24bf77d96dcee24dd9360f7079e91d
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-18 18:36:36 +0900

    :sparkles:  nickname 의 유효성 검사 추가

---
+++ backend/src/modules/userModules/user/dtos/nickname.request.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, Length, MaxLength, MinLength, Matches } from 'class-validator';
+
+const ERROR_MESSAGE = '닉네임은 특수문자를 제외한 2~8자의 영문, 한글, 숫자만 가능합니다.';
+
+export class NicknameRequestDto {
+  @Length(2, 8, { message: ERROR_MESSAGE })
+  @IsNotEmpty({ message: ERROR_MESSAGE })
+  @Matches(/^[a-zA-Z0-9가-힣]+$/, {
+    message: ERROR_MESSAGE,
+  })
+  nickname: string;
+}
--- backend/src/modules/userModules/user/user.controller.ts
+++ backend/src/modules/userModules/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from '@user/user.service';
+import { NicknameRequestDto } from './dtos/nickname.request.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('check-nickname/:nickname')
-  async checkNickname(@Param('nickname') nickname: string) {
-    return await this.userService.checkUsableNickname(nickname);
+  async checkNickname(@Param() nicknameRequestDto: NicknameRequestDto) {
+    return await this.userService.checkUsableNickname(nicknameRequestDto.nickname);
   }
 }
