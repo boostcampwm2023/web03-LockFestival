e01478e9390061ade9d912a29e11453398a60c85
JEM1224 <jmeve24@naver.com>
2023-11-18 14:48:26 +0900

    ✨ GET /auth/naver 자체  엑세스 토큰 반환
    - AuthController
      - getLogin
        - authService에서 엑세스 토큰과 유저 데이터를 가져옵니다.
        - userService에서 로그인 진행합니다.
        - 자체 엑세스 토큰 반환합니다.
      - getAccessToken : 로그인 과정에서 발급하므로 함수를 삭제했습니다.
    - UserMoudle을 appModule과 authModule에 등록했습니다.
    - UserModule에 해당 컨트롤러,서비스,레포지토리를 등록했습니다.

--- backend/src/modules/authModules/auth/auth.module.ts
+++ backend/src/modules/authModules/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { HttpModule } from '@nestjs/axios';
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
 import { JwtStrategy } from '@auth/jwt.strategy';
-
+import { UserModule } from '@user/user.module';
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, HttpModule, UserModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
