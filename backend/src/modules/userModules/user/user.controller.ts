4445b29073b81abbc933fb6e5fdccf6fa7a9f55d
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-18 22:44:23 +0900

    :sparkles: 상단바를 위한 UserProfileDto를 반환하는 API
    - GET /users/profile
    
      - need Authorization header

--- backend/src/modules/userModules/user/user.controller.ts
+++ backend/src/modules/userModules/user/user.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { TokenAuthGuard } from '@src/modules/authModules/auth/auth.guard';
 import { UserService } from '@user/user.service';
+import { UserProfileDto } from './dtos/user.profile.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get('/profile')
+  @UseGuards(TokenAuthGuard)
+  async getUserProfile(@Req() { user }): Promise<UserProfileDto> {
+    const userProfileDto: UserProfileDto = await this.userService.getUserProfileByNickname(
+      user.nickname
+    );
+    return userProfileDto;
+  }
 }
--- backend/src/modules/userModules/user/user.service.ts
+++ backend/src/modules/userModules/user/user.service.ts
@@ -3,6 +3,7 @@ import { UserRepository } from '@user/user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserNaverDto } from '@user/dtos/user.naver.dto';
 import { User } from '@user/entities/user.entity';
+import { UserProfileDto } from './dtos/user.profile.dto';
 
 @Injectable()
 export class UserService {
@@ -21,5 +22,9 @@ export class UserService {
     } catch (error) {
       throw new HttpException('Login failed.', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
+
+  async getUserProfileByNickname(nickname: string): Promise<UserProfileDto> {
+    return this.userRepository.findUserProfileByNickname(nickname);
   }
 }
