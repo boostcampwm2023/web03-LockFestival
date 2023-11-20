b5b23c216aab1f5547e9af08b68d0ad976e8a7cf
navyjeongs <wjdtjr8649@naver.com>
2023-11-21 02:01:00 +0900

    ✨ 랜덤 테마 불러오는 custom hook 및 api 작성

---
+++ frontend/src/hooks/useRandomThemesQuery.tsx
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+
+import QUERY_MANAGEMENT from '@constants/QueryManagement';
+
+const useRandomThemesQuery = () => {
+  const { data, isSuccess, isError, isLoading } = useQuery({
+    queryKey: [QUERY_MANAGEMENT['randomThemes'].key],
+    queryFn: QUERY_MANAGEMENT['randomThemes'].fn,
+  });
+
+  return { data, isSuccess, isError, isLoading };
+};
+
+export default useRandomThemesQuery;
