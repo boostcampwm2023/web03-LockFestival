e2338ed71e9f554bb54dc9fbf59a7b1eabbcfbe0
navyjeongs <wjdtjr8649@naver.com>
2023-11-21 14:08:54 +0900

    ✨ 기본  라우팅 페이지 생성

---
+++ frontend/src/pages/Mypage/Mypage.tsx
@@ -0,0 +1,5 @@
+const MyPage = () => {
+  return <div>마이페이지</div>;
+};
+
+export default MyPage;
---
+++ frontend/src/pages/Recruitment/Recruitment.tsx
@@ -0,0 +1,5 @@
+const Recruitment = () => {
+  return <div>모집 페이지</div>;
+};
+
+export default Recruitment;
