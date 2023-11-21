a315ca423ae76f1fc3e5cce981bbc0018f2ee4a4
navyjeongs <wjdtjr8649@naver.com>
2023-11-21 14:40:24 +0900

    ✨ api 요청 기본 값 및 getCurrentPosition Option 작성

---
+++ frontend/src/constants/geolocation.ts
@@ -0,0 +1,17 @@
+const REQUEST_INFO = {
+  km: 5,
+  themeCount: 10,
+};
+
+const OPTIONS = {
+  enableHighAccuracy: true,
+  timeout: 1000,
+  maximumAge: 0,
+};
+
+const HongdaePos: Pick<GeolocationCoordinates, 'latitude' | 'longitude'> = {
+  longitude: 126.97255197,
+  latitude: 37.48158005,
+};
+
+export { REQUEST_INFO, OPTIONS, HongdaePos };
