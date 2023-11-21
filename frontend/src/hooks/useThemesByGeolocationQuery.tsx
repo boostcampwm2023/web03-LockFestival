0e218cbf35e9b0eb168844fa8e1811aa8c907661
navyjeongs <wjdtjr8649@naver.com>
2023-11-21 14:43:14 +0900

    ✨ 위치 정보를 바탕으로 테마리스트를 불러오는 custom Hook 생성

---
+++ frontend/src/hooks/useThemesByGeolocationQuery.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+
+import { useQuery } from '@tanstack/react-query';
+import useGeolocation from '@hooks/useGeolocation';
+import QUERY_MANAGEMENT from '@constants/queryManagement';
+
+const INITAL_LATITUDE = 0;
+
+const useThemesByGeolocationQuery = () => {
+  const { geolocation } = useGeolocation();
+
+  const { data, refetch } = useQuery({
+    queryKey: [QUERY_MANAGEMENT['geolocation'].key],
+    queryFn: () => QUERY_MANAGEMENT['geolocation'].fn({ geolocation }),
+    enabled: false,
+  });
+
+  useEffect(() => {
+    geolocation.latitude !== INITAL_LATITUDE && refetch();
+  }, [geolocation, refetch]);
+
+  return { data };
+};
+
+export default useThemesByGeolocationQuery;
