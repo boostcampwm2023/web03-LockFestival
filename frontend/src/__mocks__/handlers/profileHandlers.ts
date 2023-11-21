253cfd6c2d1b40e019d5b18ba5c50c44a706f5e1
선예 <kss02281@gmail.com>
2023-11-21 22:09:54 +0900

    🚧 ✨ msw login api 추가 profile api 수정

--- frontend/src/__mocks__/handlers/profileHandlers.ts
+++ frontend/src/__mocks__/handlers/profileHandlers.ts
@@ -3,14 +3,21 @@ import { Profile } from 'types/profile';
 import { SERVER_URL } from '@config/server';
 
 const profileHandlers = [
-  http.get(SERVER_URL + '/users/profile', ({ cookies }) => {
-    //TODO: jwt 구현 후 미로그인 구현하기
-
-    return HttpResponse.json<Profile>({
-      nickname: '프엔',
-      isMoreInfo: true,
-      profileImageUrl: '',
-    });
+  http.get(SERVER_URL + '/users/profile', () => {
+    if (localStorage.getItem('com.naver.nid.access_token')) {
+      return HttpResponse.json<Profile>({
+        nickname: '프엔',
+        isMoreInfo: true,
+        profileImageUrl: '',
+      });
+    } else {
+      return new HttpResponse('Not found', {
+        status: 404,
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      });
+    }
   }),
 ];
 
