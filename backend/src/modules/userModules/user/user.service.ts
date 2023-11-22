d1d0ca0a7c069aa13153a856a419f51ac3f5e62d
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-23 01:34:30 +0900

    :sparkles: 닉네임 가능 문자열로 랜덤한 닉네임 생성하는 함수 구현

--- backend/src/modules/userModules/user/user.service.ts
+++ backend/src/modules/userModules/user/user.service.ts
@@ -54,4 +54,24 @@ export class UserService {
       throw new HttpException('Update user info failed.', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
+
+  //made by ChatGPT
+  private generateRandomString(length: number = 8): string {
+    let characters: string = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    // 한글 범위를 추가
+    characters += String.fromCodePoint(
+      ...Array.from({ length: 0xd7a3 - 0xac00 + 1 }, (_, i) => {
+        return i + 0xac00;
+      })
+    );
+
+    let result: string = '';
+    for (let i: number = 0; i < length; i++) {
+      const randomIndex: number = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(randomIndex);
+    }
+
+    return result;
+  }
 }
