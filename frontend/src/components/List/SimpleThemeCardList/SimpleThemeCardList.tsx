d219898e60203220afd226879b304eb9bf74321f
navyjeongs <wjdtjr8649@naver.com>
2023-11-22 21:08:13 +0900

    ✨ 테마가 없는경우에 보여줄 컴포넌트 추가

--- frontend/src/components/List/SimpleThemeCardList/SimpleThemeCardList.tsx
+++ frontend/src/components/List/SimpleThemeCardList/SimpleThemeCardList.tsx
@@ -1,12 +1,23 @@
 import SimpleThemeCard from '@components/Card/SimpleThemeCard/SimpleThemeCard';
 import { SimpleThemeCardData } from 'types/theme';
 import tw, { styled, css } from 'twin.macro';
+import { FaFaceFrown } from 'react-icons/fa6';
 
 interface SimpleThemeCardListProps {
   themes: Array<SimpleThemeCardData>;
 }
 
 const SimpleThemeCardList = ({ themes }: SimpleThemeCardListProps) => {
+  if (!themes.length) {
+    return (
+      <NoThemeContainer>
+        <FaFaceFrown size={35} />
+        <TextSpan>테마가 없어요</TextSpan>
+        <FaFaceFrown size={35} />
+      </NoThemeContainer>
+    );
+  }
+
   return (
     <Container>
       {themes.map((theme) => (
@@ -30,5 +41,18 @@ const Container = styled.div([
 ]);
 
 const CardContainer = styled.div([tw`desktop:(mx-4)`, tw`mobile:(mx-2)`]);
+
+const NoThemeContainer = styled.div([
+  tw`w-full overflow-hidden bg-gray-light text-[4rem] text-white`,
+  tw`desktop:(max-w-[102.4rem] h-[32.4rem])`,
+  tw`mobile:(max-w-[43rem] h-[21.6rem])`,
+  css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+]);
+
+const TextSpan = styled.span([tw`mx-4`]);
 
 export default SimpleThemeCardList;
