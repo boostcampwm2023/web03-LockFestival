ba38fbd4cc667ecf6564c69946d30ae19f96c71c
navyjeongs <wjdtjr8649@naver.com>
2023-11-22 21:09:04 +0900

    ✨ 쿼리 ket와 fn 추가

--- frontend/src/constants/queryManagement.ts
+++ frontend/src/constants/queryManagement.ts
@@ -1,6 +1,11 @@
+import fetchThemesByLocation from '@apis/fetchThemesByLocation';
 import fetchThemesByRandomGenres from '@apis/fetchThemesByRandomGenres';
 
 const QUERY_MANAGEMENT = {
+  geolocation: {
+    key: 'geolocation',
+    fn: fetchThemesByLocation,
+  },
   randomThemes: {
     key: 'randomThemes',
     fn: fetchThemesByRandomGenres,
