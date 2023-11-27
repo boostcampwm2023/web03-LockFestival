203857a0a9ab685413c0ef23d2460720426d0f26
JEM1224 <jmeve24@naver.com>
2023-11-28 01:19:22 +0900

    :sparkles: 부족한 정보를 추가

--- backend/src/modules/themeModules/theme/dtos/theme.detail.response.dto.ts
+++ backend/src/modules/themeModules/theme/dtos/theme.detail.response.dto.ts
@@ -1,10 +1,17 @@
+import { ThemeResponseDto } from '@theme/dtos/theme.response.dto';
+
 export class ThemeDeatailsResponseDto {
   name: string;
-  branchName: string;
+  brandBranchName: string;
   realGenre: string;
+  themeId: number;
   posterImageUrl: string;
   difficulty: number;
   minMember: number;
   maxMember: number;
+  playTime: number;
+  phone: number;
+  address: string;
   website: string;
+  otherThemes: ThemeResponseDto[];
 }
