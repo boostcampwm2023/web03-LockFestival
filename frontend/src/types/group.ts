111e5c023c9de2aabf448deede16302ddb947129
박정석 <wjdtjr8649@Naver.com>
2023-11-28 01:10:54 +0900

    🏷️ 그룹 리스트 카드 타입 정의

---
+++ frontend/src/types/group.ts
@@ -0,0 +1,10 @@
+export interface GroupProps {
+  groupId: string;
+  posterImageUrl: string;
+  branchName: string;
+  themeName: string;
+  contents: string;
+  lastChat: string;
+  lastChatTime: Date;
+  haveNewMessage: boolean;
+}
