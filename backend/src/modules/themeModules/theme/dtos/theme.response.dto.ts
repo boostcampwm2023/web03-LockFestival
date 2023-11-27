e1fac8c04ee8334e90722d31046df359b18f72d9
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-27 22:57:33 +0900

    :memo: /branches 문서 작성

--- backend/src/modules/themeModules/theme/dtos/theme.response.dto.ts
+++ backend/src/modules/themeModules/theme/dtos/theme.response.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ThemeResponseDto {
+  @ApiProperty({
+    name: 'posterImageUrl',
+    description: '테마 포스터 이미지 URL',
+    example: 'https://www.keyescape.co.kr/file/theme_info/40_a.jpg',
+  })
   posterImageUrl?: string;
+  @ApiProperty({ name: 'name', description: '테마명', example: 'BACK TO THE SCENE+' })
   name: string;
+  @ApiProperty({ name: 'themeId', description: '테마 id', example: 1 })
   themeId: number;
 
   constructor({ posterImageUrl, name, id }) {
