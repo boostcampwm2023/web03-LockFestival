80f6ca70b3e0494fca0e2b731c665d5e1813bb64
선예 <kss02281@gmail.com>
2023-11-28 17:28:49 +0900

    🤡 테마 상세정보 mock api 추가

--- frontend/src/__mocks__/browser.ts
+++ frontend/src/__mocks__/browser.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from 'msw/browser';
 import profileHandlers from './handlers/profileHandlers';
 import themesByGenreHandler from './handlers/themesByRandomHandler';
+import themeDetailsHandler from './handlers/themeDetailsHandlers';
 
-export const worker = setupWorker(...profileHandlers, ...themesByGenreHandler);
+export const worker = setupWorker(
+  ...profileHandlers,
+  ...themesByGenreHandler,
+  ...themeDetailsHandler
+);
