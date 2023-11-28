81878f68dea629161f6a6f3df8d3ad973bd2c231
navyjeongs <wjdtjr8649@naver.com>
2023-11-28 23:36:13 +0900

    🤡 mock api 생성 및 등록

--- frontend/src/__mocks__/browser.ts
+++ frontend/src/__mocks__/browser.ts
@@ -1,5 +1,10 @@
 import { setupWorker } from 'msw/browser';
 import profileHandlers from './handlers/profileHandlers';
 import themesByGenreHandler from './handlers/themesByRandomHandler';
+import recruitmentCardHandler from './handlers/recruitmentCardHandler';
 
-export const worker = setupWorker(...profileHandlers, ...themesByGenreHandler);
+export const worker = setupWorker(
+  ...profileHandlers,
+  ...themesByGenreHandler,
+  ...recruitmentCardHandler
+);
--- frontend/src/__mocks__/server.ts
+++ frontend/src/__mocks__/server.ts
@@ -2,5 +2,11 @@ import { setupServer } from 'msw/node';
 import profileHandlers from './handlers/profileHandlers';
 import themesByGenreHandler from './handlers/themesByRandomHandler';
 import loginHandlers from './handlers/loginHandlers';
+import recruitmentCardHandler from './handlers/recruitmentCardHandler';
 
-export const server = setupServer(...profileHandlers, ...themesByGenreHandler, ...loginHandlers);
+export const server = setupServer(
+  ...profileHandlers,
+  ...themesByGenreHandler,
+  ...loginHandlers,
+  ...recruitmentCardHandler
+);
