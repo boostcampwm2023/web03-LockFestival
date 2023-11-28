da44be50a2cc27cdf735d57d3a3c102b12a39f07
선예 <kss02281@gmail.com>
2023-11-28 23:51:25 +0900

    💄 ✨ 채팅방 페이지 중앙 섹션 마크업

---
+++ frontend/src/pages/Chat/components/ChatContainer.tsx
@@ -0,0 +1,99 @@
+import tw, { styled, css } from 'twin.macro';
+import Button from '@components/Button/Button';
+import { FaRightFromBracket } from 'react-icons/fa6';
+import useInput from '@hooks/useInput';
+import { useNavigate } from 'react-router-dom';
+
+const ChatContainer = () => {
+  const navigate = useNavigate();
+  const [inputValue, handleValue, resetValue] = useInput('');
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
+
+  const preventDefault = (e: React.FocusEvent<HTMLTextAreaElement>) => {
+    e.preventDefault();
+  };
+
+  const handleSubmit = () => {
+    //TODO: emit chat
+    resetValue();
+  };
+
+  return (
+    <Layout>
+      <ButtonWrapper>
+        <Button
+          isIcon={true}
+          onClick={() => {
+            //TODO: emit leave
+            navigate('/group-chat');
+          }}
+        >
+          <FaRightFromBracket />
+        </Button>
+      </ButtonWrapper>
+      <ChatDisplayContainer></ChatDisplayContainer>
+      <InputChatContainer
+        value={inputValue}
+        onChange={handleValue}
+        rows={4}
+        onKeyDown={handleKeyDown}
+        onFocus={preventDefault}
+        onBlur={preventDefault}
+      />
+      <SendButtonWrapper>
+        <Button type="button" isIcon={false} onClick={handleSubmit}>
+          <>보내기</>
+        </Button>
+      </SendButtonWrapper>
+    </Layout>
+  );
+};
+
+export default ChatContainer;
+const Layout = styled.div([
+  css`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 1.6rem;
+    width: 49rem;
+    height: 100vh;
+    padding: 2rem;
+  `,
+  tw`bg-gray-light rounded-[2rem] h-[calc(90vh - 6rem)]`,
+]);
+
+const ButtonWrapper = styled.div([
+  css`
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+  `,
+]);
+
+const ChatDisplayContainer = styled.div([tw`w-[100%] h-[100%] bg-white rounded-[2rem]`]);
+
+const InputChatContainer = styled.textarea([
+  tw`font-pretendard text-white text-m w-[100%] h-[10rem] bg-gray-dark rounded-[2rem] p-4`,
+  css`
+    resize: none;
+
+    &:focus {
+      outline: none;
+    }
+  `,
+]);
+
+const SendButtonWrapper = styled.div([
+  css`
+    position: absolute;
+    bottom: 3rem;
+    right: 3rem;
+  `,
+]);
