817fbf9a6a107dcb5b17018c0994777aa9a369d5
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-29 20:09:08 +0900

    :sparkles: ChatModule에 스키마 등록

--- backend/src/modules/userModules/chat/chat.module.ts
+++ backend/src/modules/userModules/chat/chat.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatController } from '@chat/chat.controller';
 import { ChatService } from '@chat/chat.service';
 import { ChatRepository } from '@chat/chat.repository';
 import { GroupModule } from '@group/group.module';
+import { Room, RoomSchema } from '@chat/entities/room.schema';
+import { ChatUser, ChatUserSchema } from '@chat/entities/chat.user.schema';
+import { ChatMessage, ChatMessageSchema } from '@chat/entities/chat.message.schema';
 
 @Module({
-  imports: [GroupModule],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Room.name, schema: RoomSchema },
+      { name: ChatUser.name, schema: ChatUserSchema },
+      { name: ChatMessage.name, schema: ChatMessageSchema },
+    ]),
+    GroupModule,
+  ],
   controllers: [ChatController],
   providers: [ChatService, ChatRepository],
 })
