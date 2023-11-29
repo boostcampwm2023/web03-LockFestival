4426adc2e2e920652a5cadd6ce5151514dc3fa2d
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-30 02:03:52 +0900

    :bug: GroupModule <-> ChatModule 순환 의존성 해결

--- backend/src/modules/userModules/group/group.module.ts
+++ backend/src/modules/userModules/group/group.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from '@group/entities/group.entity';
 import { GroupController } from '@group/group.controller';
@@ -6,10 +6,19 @@ import { GroupService } from '@group/group.service';
 import { GroupRepository } from '@group/group.repository';
 import { UserModule } from '@user/user.module';
 import { ThemeModule } from '@theme/theme.module';
+import { ChatModule } from '@chat/chat.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group]), UserModule, ThemeModule],
+  imports: [
+    TypeOrmModule.forFeature([Group]),
+    UserModule,
+    ThemeModule,
+    forwardRef(() => {
+      return ChatModule;
+    }),
+  ],
   controllers: [GroupController],
   providers: [GroupService, GroupRepository],
+  exports: [GroupRepository],
 })
 export class GroupModule {}
