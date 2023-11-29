98fde0d1ba98aba6465443c2f96d99136c77f6d2
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-30 03:05:30 +0900

    :sparkles: 채팅방에 사용자가 속하는지 검증하고 이에 필요한 데이터 반환 및 채팅방의 유저 정보 반환
    - lastChatLogId: 접속 전 마지막으로 읽은 로그 아이디
    - unreadCountMap: 렌더링을 위한 채팅방 안읽은 숫자의 포인터 로그 아이디
    - chatUsers: 채팅방의 유저 정보

--- backend/src/modules/userModules/chat/chat.service.ts
+++ backend/src/modules/userModules/chat/chat.service.ts
@@ -4,4 +4,44 @@ import { ChatRepository } from '@chat/chat.repository';
 @Injectable()
 export class ChatService {
   constructor(private readonly chatRepository: ChatRepository) {}
+
+  async validateRoomAndGetChatUserList(roomId: string, nickname: string) {
+    const chatUsers = (
+      await this.chatRepository.validateRoomAndGetChatUserList(roomId, nickname)
+    ).map((chatUser) => {
+      return {
+        ...chatUser,
+        isMe: chatUser.user_nickname === nickname,
+      };
+    });
+
+    const lastChatLogId: string = chatUsers.find(({ isMe }) => {
+      return isMe;
+    }).last_chat_log_id;
+
+    const countMap: object = chatUsers
+      .filter((user) => {
+        return !user.isMe;
+      })
+      .map(({ last_chat_log_id: lastChatLogId }) => {
+        return lastChatLogId.toString();
+      })
+      .reduce((acc, cur) => {
+        acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
+        return acc;
+      }, {});
+
+    let count: number = 0;
+    const unreadCountMap = Object.entries(countMap)
+      .sort(([key1], [key2]) => {
+        return key1.localeCompare(key2);
+      })
+      .reduce((acc, [key, value]) => {
+        count += value;
+        acc[count] = key;
+        return acc;
+      }, {});
+
+    return { lastChatLogId, unreadCountMap, chatUsers };
+  }
 }
