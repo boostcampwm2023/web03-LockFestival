7b794022d7dbebdff6559c8460dc4d6634b8f83e
kangchanghan <anycar8515@ynu.ac.kr>
2023-12-01 02:01:47 +0900

    :sparkles: 인메모리 세션 구현 및 채팅 생성 로직 변경

--- backend/src/modules/userModules/chat/dtos/chat.message.request.dto.ts
+++ backend/src/modules/userModules/chat/dtos/chat.message.request.dto.ts
@@ -5,4 +5,11 @@ export class ChatMessageRequestDto {
   groupId: number;
   type: ChatType;
   time: Date;
+  constructor(message: string, userId: string, groupId: number, type: ChatType, time: Date) {
+    this.message = message;
+    this.userId = userId;
+    this.groupId = groupId;
+    this.type = type;
+    this.time = time;
+  }
 }
