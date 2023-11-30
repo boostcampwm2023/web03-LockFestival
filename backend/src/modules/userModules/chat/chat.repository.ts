cc11a148d6f0aaf635a8351ccfee0e2548cba730
JEM1224 <jmeve24@naver.com>
2023-11-30 14:49:25 +0900

    :art: 보낸사람이 저장안되는 에러 수정
    보낸사람이 저장이 안되는 에러를 수정하고 불러올 때 보낸사람을 추가했어요

--- backend/src/modules/userModules/chat/chat.repository.ts
+++ backend/src/modules/userModules/chat/chat.repository.ts
@@ -4,6 +4,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { ChatUser } from '@chat/entities/chat.user.schema';
 import { ChatMessage } from '@chat/entities/chat.message.schema';
 import { Room } from '@chat/entities/room.schema';
+import { ChatUnreadDto } from '@chat/dtos/chat.unread.dto';
+import { ChatMessageDto } from '@chat/dtos/chat.message.dto';
+import { ChatMessageResponseDto } from '@chat/dtos/chat.message.response.dto';
 
 @Injectable()
 export class ChatRepository {
@@ -13,25 +16,20 @@ export class ChatRepository {
     @InjectModel(ChatUser.name) private chatUserModel: Model<ChatUser>
   ) {}
 
-  async createMessgeByChat({ message, type, time, userId, groupId }) {
-    const chat = await this.chatMessageModel
-      .create({
-        chat_message: message,
-        type: type,
-        chat_date: time,
-      })
-      .then((chatMessage) => {
-        return chatMessage.populate({
-          path: 'sender',
-          model: 'ChatUser',
-          match: { user_id: userId },
-        });
-      });
+  async createMessgeByChat(chatMessageDto: ChatMessageDto): Promise<ChatMessageResponseDto> {
+    const chatUser = await this.chatUserModel.findOne({ user_id: chatMessageDto.userId });
+
+    const chat = await this.chatMessageModel.create({
+      chat_message: chatMessageDto.message,
+      sender: chatUser,
+      type: chatMessageDto.type,
+      chat_date: chatMessageDto.time,
+    });
 
     await this.roomModel
       .updateOne(
         {
-          group_id: groupId,
+          group_id: chatMessageDto.groupId,
         },
         {
           $push: {
@@ -40,18 +38,26 @@ export class ChatRepository {
         }
       )
       .exec();
+
+    return new ChatMessageResponseDto(chat);
   }
-  async findMessagesByStartLogId(roomId: string, startLogId: string, count: number) {
+  async findMessagesByStartLogId(chatUnreadDto: ChatUnreadDto): Promise<ChatMessageResponseDto[]> {
     return (
-      await this.roomModel.findOne({ group_id: 1 }).populate({
+      await this.roomModel.findOne({ group_id: chatUnreadDto.roomId }).populate({
         path: 'chat_list',
         model: 'ChatMessage',
-        match: { _id: { $gt: startLogId } },
+        match: { _id: { $gt: chatUnreadDto.startLogId } },
         options: {
           sort: { _id: 1 },
-          // limit: count,
+        },
+        populate: {
+          path: 'sender',
+          model: 'ChatUser',
+          select: 'user_nickname',
         },
       })
-    ).chat_list;
+    ).chat_list.map((message) => {
+      return new ChatMessageResponseDto(message);
+    });
   }
 }
--- backend/src/modules/userModules/chat/chat.service.ts
+++ backend/src/modules/userModules/chat/chat.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { ChatRepository } from '@chat/chat.repository';
+import { ChatMessageDto } from '@chat/dtos/chat.message.dto';
+import { ChatUnreadDto } from '@chat/dtos/chat.unread.dto';
+import { ChatMessageResponseDto } from '@chat/dtos/chat.message.response.dto';
 
 @Injectable()
 export class ChatService {
   constructor(private readonly chatRepository: ChatRepository) {}
+
+  async createMessgeByChat(chatMessageDto: ChatMessageDto): Promise<ChatMessageResponseDto> {
+    return await this.chatRepository.createMessgeByChat(chatMessageDto);
+  }
+  async findMessagesByLogId(chatUnreadDto: ChatUnreadDto): Promise<ChatMessageResponseDto[]> {
+    return await this.chatRepository.findMessagesByStartLogId(chatUnreadDto);
+  }
 }
