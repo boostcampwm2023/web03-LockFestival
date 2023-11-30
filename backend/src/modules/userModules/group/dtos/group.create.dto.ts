67b9ce546055b758fabf11bb8f83d03cb4d83350
JEM1224 <jmeve24@naver.com>
2023-11-30 23:37:08 +0900

    ✏️ 오타 수정
    Co-authored-by: 강창한 <anycar8515@ynu.ac.kr>

--- backend/src/modules/userModules/group/dtos/group.create.dto.ts
+++ backend/src/modules/userModules/group/dtos/group.create.dto.ts
@@ -8,8 +8,8 @@ export class GroupRequestDto {
     example: '(테마 변경 가능)11월 5일 오후 5시에 갈사람 구해요. ',
   })
   recruitmentContent: string;
-  @ApiProperty({ description: '모집 완료 여부', example: false })
-  recruitmentCompleted: boolean;
+  @ApiProperty({ description: '예약 완료 여부', example: false })
+  appointmentCompleted: boolean;
   @ApiProperty({ description: '예약 날짜' })
   appointmentDate: Date;
   @ApiProperty({ description: '모집하는 테마의 테마 id', example: 1 })
