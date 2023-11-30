19e0503dc1b92254a8a1c238003552d32d176b84
kangchanghan <anycar8515@ynu.ac.kr>
2023-11-30 17:13:56 +0900

    :wrench: redis와 socket.io를 연결하는 adapter 구현
    Co-authored-by: JEM1224 <jmeve24@naver.com>

---
+++ backend/src/adapter/redis.io.adapter.ts
@@ -0,0 +1,30 @@
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createClient } from 'redis';
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+export class RedisIoAdapter extends IoAdapter {
+  private adapterConstructor: ReturnType<typeof createAdapter>;
+  constructor(
+    app: INestApplication,
+    private readonly configService: ConfigService
+  ) {
+    super(app);
+  }
+  async connectToRedis(): Promise<void> {
+    const pubClient = createClient({ url: this.configService.get('REDIS_URL') });
+    const subClient = pubClient.duplicate();
+
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+
+    this.adapterConstructor = createAdapter(pubClient, subClient);
+  }
+
+  createIOServer(port: number, options?: ServerOptions): any {
+    const server = super.createIOServer(port, options);
+    server.adapter(this.adapterConstructor);
+    return server;
+  }
+}
