b083499abd418929f6aee033b9c6a7ee9e38ab6f
선예 <kss02281@gmail.com>
2023-12-02 14:28:59 +0900

    ✨ useSocket : 상단에서 하위 훅을 import 해서 합쳐주고, chat 이벤트 dispatcher, listener 등록하는 커스텀 훅

--- frontend/src/hooks/socket/useSocket.ts
+++ frontend/src/hooks/socket/useSocket.ts
@@ -1,97 +1,28 @@
-import { useEffect, useState } from 'react';
-import { io, Socket } from 'socket.io-client';
-import { SOCKET_URL } from '@config/server';
-import { RoomInfo, UserInfo } from 'types/chat';
-import useRoomInfo from './useRoomInfo';
-import useUserListInfo from './useUserInfo';
-import { useRecoilState } from 'recoil';
-import { ServerChatLog, TotalChatLog, ChatLog } from 'types/chat';
-import { chatLogAtom } from 'store/chatRoom';
+import { useState, useEffect } from 'react';
+import { ServerChatLog } from 'types/chat';
+import useSocketConnection from './useSocketConnection';
+import useChatLog from './useChatLog';
 
-//TODO: useSocket 분리하기
 const useSocket = (roomId: string) => {
-  const accessToken = localStorage.getItem('accessToken');
-  const [chatLog, setChatLog] = useRecoilState<TotalChatLog>(chatLogAtom);
+  const { socket, connecting } = useSocketConnection(roomId);
+  const [receiveChat, setReceiveChat] = useState<ServerChatLog | null>(null);
 
-  const [receiveChat, setReceiveChat] = useState<ServerChatLog>();
+  useChatLog(roomId, receiveChat);
 
-  const { roomInfo, setRoomInfo } = useRoomInfo();
-  const { userListInfo, setUserListInfo } = useUserListInfo();
-
-  const [socket, setSocket] = useState<Socket>();
+  useEffect(() => {
+    if (socket) {
+      socket.on('chat', (res: ServerChatLog) => {
+        const { message, userId, type, time, chatId } = res;
+        setReceiveChat({ message, userId, type, time, chatId });
+      });
+    }
+  }, [socket]);
 
   const sendChat = (message: string) => {
     socket?.emit('chat', { message }, (res: any) => {});
   };
 
-  const connectSocket = () => {
-    socket?.on('connect', () => {
-      joinRoom();
-    });
-  };
-
-  const joinRoom = () => {
-    socket?.emit(
-      'join',
-      {
-        roomId,
-        Authorization: `${accessToken}`,
-      },
-      (res: any) => {
-        if (res === 'ok') {
-          console.log('방에 접속 성공');
-        }
-      }
-    );
-
-    socket?.on('roomInfo', (res: RoomInfo) => {
-      setRoomInfo(res);
-    });
-
-    socket?.on('userListInfo', (res: UserInfo[]) => {
-      setUserListInfo(res);
-    });
-
-    //TODO: 에러 처리하기
-    socket?.on('error', (err) => {
-      console.log(err, '에러발생');
-      socket?.disconnect();
-    });
-
-    socket?.on('chat', (res: ServerChatLog) => {
-      const { message, userId, type, time, chatId } = res;
-      setReceiveChat({ message, userId, type, time, chatId });
-    });
-  };
-
-  useEffect(() => {
-    const currentRoomChatting = new Map<string, ChatLog>(chatLog[roomId]);
-
-    if (receiveChat) {
-      const { chatId, message, userId, type, time } = receiveChat;
-
-      currentRoomChatting.set(chatId, {
-        message,
-        userId,
-        type,
-        time,
-      });
-      setChatLog((prev) => ({ ...prev, [roomId]: currentRoomChatting }));
-    }
-  }, [receiveChat]);
-
-  useEffect(() => {
-    connectSocket();
-  }, [socket]);
-
-  useEffect(() => {
-    const disposableSocket = io(`${SOCKET_URL}`, {
-      reconnectionDelayMax: 10000,
-    });
-    setSocket(disposableSocket);
-  }, []);
-
-  return { roomInfo, userListInfo, sendChat };
+  return { sendChat, connecting };
 };
 
 export default useSocket;
