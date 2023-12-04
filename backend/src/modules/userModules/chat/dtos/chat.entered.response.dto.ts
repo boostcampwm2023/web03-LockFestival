f4696a837844310dd0d3cea92f0a17be6aa8c996
kangchanghan <anycar8515@ynu.ac.kr>
2023-12-05 01:54:30 +0900

    :memo: API 명세 추가

--- backend/src/modules/userModules/chat/dtos/chat.entered.response.dto.ts
+++ backend/src/modules/userModules/chat/dtos/chat.entered.response.dto.ts
@@ -1,7 +1,14 @@
 import { ChatMessageDto } from '@chat/dtos/chat.message.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class EnteredChatMessageRequestDto {
+  @ApiProperty({
+    type: [ChatMessageDto],
+  })
   messages: ChatMessageDto[];
+  @ApiProperty({
+    description: '커서로 사용한 안읽은 chatLogId',
+  })
   cursorLogId: string;
 
   constructor(cursorLogId: string, messages: ChatMessageDto[]) {
