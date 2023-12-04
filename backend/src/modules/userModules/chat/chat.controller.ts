7b9f41e2d2a9c0e18ce5c2286fbe2d5b4afd6bc1
kangchanghan <anycar8515@ynu.ac.kr>
2023-12-05 01:53:39 +0900

    :sparkles: GET /chat/:groupId/unread API 추가
    - 특정 유저의 특정 채팅방의 안읽은 메세지를 반환하는 API 개발

--- backend/src/modules/userModules/chat/chat.controller.ts
+++ backend/src/modules/userModules/chat/chat.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { ChatService } from '@chat/chat.service';
+import { TokenAuthGuard } from '@auth/auth.guard';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { EnteredChatMessageRequestDto } from '@chat/dtos/chat.entered.response.dto';
 
-@Controller('chat/')
+@Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+  @Get(':groupId/unread')
+  @UseGuards(TokenAuthGuard)
+  @ApiOperation({
+    description: '채팅방 입장시 안읽은 메세지 리스트를 반환합니다.',
+  })
+  @ApiBadRequestResponse({
+    description: '유저가 방에 없습니다.',
+  })
+  @ApiBearerAuth('Authorization')
+  async getUnreadChatListByGroupId(
+    @Req() { user },
+    @Param('groupId') groupId: number
+  ): Promise<EnteredChatMessageRequestDto> {
+    return await this.chatService.findUnreadMessagesByRoomIdAndNickname(
+      String(groupId),
+      user.nickname
+    );
+  }
 }
