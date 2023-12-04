8f19137b65ecce4192984bb0b229ea1a05c7737c
선예 <kss02281@gmail.com>
2023-12-04 09:52:27 +0900

    💄 방정보 상태 표기 변경

--- frontend/src/pages/ChatRoom/components/RoomInfoPanel/StateLabel/StateLabel.tsx
+++ frontend/src/pages/ChatRoom/components/RoomInfoPanel/StateLabel/StateLabel.tsx
@@ -1,8 +1,5 @@
 import Label from '@components/Label/Label';
 import { styled, css } from 'twin.macro';
-import { FaRegCircleCheck } from 'react-icons/fa6';
-import { FaRegCircleXmark } from 'react-icons/fa6';
-
 interface StateLabelProps {
   text: string;
   state: boolean;
@@ -10,10 +7,10 @@ interface StateLabelProps {
 
 const StateLabel = ({ text, state }: StateLabelProps) => {
   return (
-    <Label isBorder={true} backgroundColor={state ? 'green-light' : 'green-dark'} width="10rem">
+    <Label isBorder={true} backgroundColor={state ? 'green-dark' : 'green-light'} width="10rem">
       <LabelText>
         {text}
-        {state ? <FaRegCircleCheck /> : <FaRegCircleXmark />}
+        {state ? '완료' : '중'}
       </LabelText>
     </Label>
   );
