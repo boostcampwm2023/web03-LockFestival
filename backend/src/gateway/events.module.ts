c974c184aa48e9e6ffee9d6ef3d07ce93b120c4a
JEM1224 <jmeve24@naver.com>
2023-12-05 22:35:05 +0900

    🐛 순환참조 에러 해결
    - EventsGateway를 주입할 때 순환 참조 에러 발생

--- backend/src/gateway/events.module.ts
+++ backend/src/gateway/events.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef, Global } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { EventsGateway } from '@src/gateway/events.gateway';
 import { ChatModule } from '@chat/chat.module';
 import { GroupModule } from '@group/group.module';
 
 @Module({
-  imports: [ChatModule, JwtModule, GroupModule],
+  imports: [
+    forwardRef(() => {
+      return ChatModule;
+    }),
+    forwardRef(() => {
+      return GroupModule;
+    }),
+    JwtModule,
+  ],
   providers: [EventsGateway],
+  exports: [EventsGateway],
 })
 export class EventsModule {}
