e9dfe003431217e31bab77efdbc4040086a74360
navyjeongs <wjdtjr8649@naver.com>
2023-12-06 00:02:30 +0900

    ✨ 안읽은 로그 아이디를 저장하는 전역 상태 추가 및 소켓 이벤트 추가

--- frontend/src/hooks/socket/useSocket.ts
+++ frontend/src/hooks/socket/useSocket.ts
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { ServerChatLog, ServerChatLogList } from 'types/chat';
 import useSocketConnection from './useSocketConnection';
 import useChatLog from './useChatLog';
+import { useSetRecoilState } from 'recoil';
+import { chatUnreadAtom } from '@store/chatRoom';
 
 const useSocket = (roomId: string) => {
   const { socket, connecting } = useSocketConnection(roomId);
@@ -11,6 +13,8 @@ const useSocket = (roomId: string) => {
   const [receivePastChat, setReceivePastChat] = useState<ServerChatLogList | null>(null);
 
   const { addPresentChat, addPastChats, addFutureChats } = useChatLog(roomId);
+
+  const setChatUnreadAtom = useSetRecoilState(chatUnreadAtom);
 
   const sendChat = (message: string) => {
     socket?.emit('chat', { message });
@@ -33,6 +37,10 @@ const useSocket = (roomId: string) => {
 
       socket.on('chatLog', (res: ServerChatLogList) => {
         setReceivePastChat(res);
+      });
+
+      socket.on('unread', (res: Record<string, string>) => {
+        setChatUnreadAtom(new Map(Object.entries(res)));
       });
     }
   }, [socket]);
