217c6b4c5f9fabf3836db4b3bb18eeb59fd87e21
선예 <kss02281@gmail.com>
2023-12-05 18:14:37 +0900

    💄 ✨ 카드 밑줄 버튼 컴포넌트 생성

---
+++ frontend/src/pages/Search/components/Card/UnderLineButton/UnderLineButton.tsx
@@ -0,0 +1,34 @@
+import tw, { styled, css } from 'twin.macro';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: JSX.Element;
+  onClick?: () => void;
+}
+
+const UnderLineButton = ({ children, onClick }: ButtonProps) => {
+  return <UnderlineButton onClick={onClick}>{children}</UnderlineButton>;
+};
+
+export default UnderLineButton;
+
+const UnderlineButton = styled.button([
+  tw`font-pretendard text-l text-white bg-transparent`,
+  css`
+    cursor: pointer;
+
+    :after {
+      display: block;
+      content: '';
+      border-bottom: solid 2px white;
+      transform: scale(0);
+      transition: transform 250ms ease-in-out;
+    }
+
+    :hover:after {
+      transform: scaleX(1);
+    }
+    :after {
+      transform-origin: top left;
+    }
+  `,
+]);
