74837fe6d38f61c6629db62408429bd16deefb85
navyjeongs <wjdtjr8649@naver.com>
2023-12-06 00:07:41 +0900

    ✨ 안읽은 수를 계산하는 로직

--- frontend/src/pages/ChatRoom/components/ChatPanel/MessageBox/MessageBox.tsx
+++ frontend/src/pages/ChatRoom/components/ChatPanel/MessageBox/MessageBox.tsx
@@ -1,12 +1,17 @@
 import { styled, css } from 'twin.macro';
 import { ChatLog } from 'types/chat';
-import { userListInfoAtom } from '@store/chatRoom';
+import { chatUnreadAtom, userListInfoAtom } from '@store/chatRoom';
 import { useRecoilValue } from 'recoil';
 import MyChatBox from './BoxType/MyChatBox';
 import OtherChatBox from './BoxType/OtherChatBox';
 import SystemChatBox from './BoxType/SystemChatBox';
+import { useMemo } from 'react';
+interface Props extends ChatLog {
+  logId: string;
+}
 
-const MessageBox = ({ message, userId, type, time }: ChatLog) => {
+const MessageBox = ({ logId, message, userId, type, time }: Props) => {
+  const chatUnread = useRecoilValue(chatUnreadAtom);
   const userData = useRecoilValue(userListInfoAtom);
   const myData = userData?.get(userId) || {
     nickname: '알 수 없는 사용자',
@@ -15,15 +20,46 @@ const MessageBox = ({ message, userId, type, time }: ChatLog) => {
   };
   const { nickname, isMe, profileImg } = myData;
 
+  const unreadCount = useMemo(() => {
+    if (!chatUnread) {
+      return 0;
+    }
+    const mapArr = new Array(...chatUnread).sort(([key1], [key2]) => {
+      return key1.localeCompare(key2);
+    });
+
+    for (let i = 0; i < mapArr.length; i++) {
+      if (logId <= mapArr[i][1]) {
+        if (i === 0) {
+          return 0;
+        }
+        return Number(mapArr[i - 1][0]);
+      }
+    }
+
+    if (mapArr.length === 0) {
+      return 0;
+    }
+
+    return Number(mapArr[mapArr.length - 1][0]);
+  }, [chatUnread]);
+
   // 같은 년 월 일, 시각 분, 사용자가 같으면 하나의 nickname만
 
   return (
     <Layout type={type} isMe={isMe}>
       {type !== 'message' && <SystemChatBox message={message} />}
-      {type === 'message' && isMe ? (
-        <MyChatBox message={message} time={time} />
-      ) : (
-        <OtherChatBox message={message} time={time} nickname={nickname} profileImg={profileImg} />
+      {type === 'message' && isMe && (
+        <MyChatBox message={message} time={time} unreadCount={unreadCount} />
+      )}
+      {type === 'message' && !isMe && (
+        <OtherChatBox
+          message={message}
+          time={time}
+          nickname={nickname}
+          profileImg={profileImg}
+          unreadCount={unreadCount}
+        />
       )}
     </Layout>
   );
