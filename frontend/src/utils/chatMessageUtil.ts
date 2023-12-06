2911ab78e41993ed71991004e47cfa3226e8f2b3
선예 <kss02281@gmail.com>
2023-12-06 12:03:09 +0900

    ✨ 메세지의 프로필 표시 여부결정 유틸 함수
    - 같은 시각의 첫번째 채팅일 경우
    - 같은 시각이지만 사용자가 다를 경우

--- frontend/src/utils/chatMessageUtil.ts
+++ frontend/src/utils/chatMessageUtil.ts
@@ -1,5 +1,5 @@
 import { ChatLog } from 'types/chat';
-import { checkDateIsSameDate } from './dateUtil';
+import { checkDateIsSameDate, getTimeByDate } from './dateUtil';
 
 // 특정 날짜의 첫 번째 채팅인지 확인한다.
 const checkIsFirstChatToday = (targetIndex: number, chatLogData: [string, ChatLog][]): boolean => {
@@ -11,4 +11,34 @@ const checkIsFirstChatToday = (targetIndex: number, chatLogData: [string, ChatLo
   return checkDateIsSameDate(new Date(prevTime), new Date(targetTime));
 };
 
-export { checkIsFirstChatToday };
+// 프로필 표시 여부
+const checkIsFirstChatFromUser = (
+  targetIndex: number,
+  chatLogData: [string, ChatLog][]
+): boolean => {
+  if (targetIndex === 0) {
+    return true;
+  }
+
+  const prevData = chatLogData[targetIndex - 1][1];
+  const targetData = chatLogData[targetIndex][1];
+
+  const prevUser = prevData.userId;
+  const prevTime = getTimeByDate(new Date(prevData.time));
+  const targetUser = targetData.userId;
+  const targetTime = getTimeByDate(new Date(targetData.time));
+
+  // 특정 유저의 채팅이 해당 시각에 첫 번째 채팅이면
+  if (prevTime !== targetTime) {
+    return true;
+  }
+
+  // 같은 시각이지만 사용자가 다르면
+  if (prevUser !== targetUser) {
+    return true;
+  }
+
+  return false;
+};
+
+export { checkIsFirstChatToday, checkIsFirstChatFromUser };
