be690bb5f7a9d94f68ea3c2e1dc3a4cb14954d01
선예 <kss02281@gmail.com>
2023-12-08 03:46:48 +0900

    🤡 타임 테이블 mock데이터 추가

---
+++ frontend/src/pages/ChatRoom/mock/mockTimeTableData.ts
@@ -0,0 +1,11 @@
+import { TimeTable } from 'types/chat';
+
+export const mockTimeTableData: TimeTable[] = [
+  { time: '14:50', possible: true },
+  { time: '16:00', possible: true },
+  { time: '17:10', possible: false },
+  { time: '18:20', possible: true },
+  { time: '19:30', possible: true },
+  { time: '20:40', possible: false },
+  { time: '21:50', possible: true },
+];
