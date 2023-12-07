ae13e5aec71b345fc0f9f259732b01783c1afe65
선예 <kss02281@gmail.com>
2023-12-08 02:17:37 +0900

    💄검색 입력 값 설명 css 조정

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -50,8 +50,12 @@ const Search = () => {
 export default Search;
 
 const Layout = styled.div([
-  tw`w-full mx-auto mt-[4rem] pb-[4rem]`,
+  tw`w-full mx-auto mt-[10rem] pb-[4rem]`,
+  tw`desktop:(max-w-[102.4rem])`,
+  tw`tablet:(max-w-[78rem])`,
+  tw`mobile:(max-w-[80%])`,
   css`
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -59,7 +63,15 @@ const Layout = styled.div([
   `,
 ]);
 
-const InfoText = styled.div([tw`font-maplestory text-xl text-white mb-6 self-start ml-[12rem]`]);
+const InfoText = styled.div([
+  tw`font-maplestory text-xl text-white self-start`,
+  tw`desktop:(top-[-4rem] left-[4rem] mb-6)`,
+  tw`tablet:(top-[-4rem] left-[15rem] mb-4)`,
+  tw`mobile:(top-[-4rem] left-[2rem])`,
+  css`
+    position: absolute;
+  `,
+]);
 
 const CardList = styled.div([
   tw`w-full desktop:(max-w-[97rem]) tablet:(flex-col) mobile:(flex-col min-w-[34rem])`,
