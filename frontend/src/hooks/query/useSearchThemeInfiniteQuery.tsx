624d4c8d036914bf5aa5864a37c02e8ac515b41c
선예 <kss02281@gmail.com>
2023-12-08 00:41:06 +0900

    🐛  쿼리파라미터 쿼리키에 추가
    - 기존에 쿼리스트링이 변경되어도 중복된 결과가 나오거나 API요청을 하지 않았던 버그를 해결했어요.

--- frontend/src/hooks/query/useSearchThemeInfiniteQuery.tsx
+++ frontend/src/hooks/query/useSearchThemeInfiniteQuery.tsx
@@ -3,13 +3,12 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 
 const useSearchThemeInfiniteQuery = (query: string) => {
   const { data, error, fetchNextPage, hasNextPage, isFetching, refetch } = useInfiniteQuery({
-    queryKey: [QUERY_MANAGEMENT['themeSearchResult'].key],
+    queryKey: [QUERY_MANAGEMENT['themeSearchResult'].key, query],
     queryFn: ({ pageParam }) => QUERY_MANAGEMENT['themeSearchResult'].fn({ pageParam, query }),
     initialPageParam: 1,
     getNextPageParam: (lastPage) => {
       return lastPage._meta.nextPage;
     },
-    enabled: query !== '',
   });
 
   return { data, fetchNextPage, hasNextPage, error, isFetching, refetch };
