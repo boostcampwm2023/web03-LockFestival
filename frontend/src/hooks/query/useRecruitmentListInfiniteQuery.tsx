07feea64246795810d1a9102295b999caa14a192
선예 <kss02281@gmail.com>
2023-12-07 22:41:21 +0900

    ✨ 모집글 쿼리 필터 적용 및 쿼리키 추가

--- frontend/src/hooks/query/useRecruitmentListInfiniteQuery.tsx
+++ frontend/src/hooks/query/useRecruitmentListInfiniteQuery.tsx
@@ -1,11 +1,22 @@
 import QUERY_MANAGEMENT from '@constants/queryManagement';
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 
 const useRecruitmentListInfiniteQuery = () => {
+  const [searchParams] = useSearchParams();
+  const themeName = searchParams.get('themeName') || '';
+  const smallRegion = searchParams.get('smallRegion') || '';
+  const bigRegion = searchParams.get('bigRegion') || '';
+
   const { data, error, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
-    queryKey: [QUERY_MANAGEMENT['recruitmentList'].key],
+    queryKey: [QUERY_MANAGEMENT['recruitmentList'].key, themeName + bigRegion + smallRegion],
     queryFn: ({ pageParam }) =>
-      QUERY_MANAGEMENT['recruitmentList'].fn({ cursorGroupId: pageParam }),
+      QUERY_MANAGEMENT['recruitmentList'].fn({
+        cursorGroupId: pageParam,
+        themeName,
+        smallRegion,
+        bigRegion,
+      }),
     initialPageParam: -1,
     getNextPageParam: (lastPage) => {
       return lastPage._meta.nextCursor;
