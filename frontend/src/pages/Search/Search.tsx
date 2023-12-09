d6590b438eb22cfc517f4ed0954bc8fc74246e97
선예 <kss02281@gmail.com>
2023-12-10 01:02:04 +0900

    ✨ 이전 페이지로 돌아가기 버튼 생성

--- frontend/src/pages/Search/Search.tsx
+++ frontend/src/pages/Search/Search.tsx
@@ -1,17 +1,19 @@
 import tw, { styled, css } from 'twin.macro';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import useSearchThemeInfiniteQuery from '@hooks/query/useSearchThemeInfiniteQuery';
 import Card from './components/Card';
 import { ThemeDetailsData } from 'types/theme';
 import { useEffect, useRef } from 'react';
 import useIntersectionObserver from '@hooks/intersectionObserver/useIntersectionObserver';
+import Button from '@components/Button/Button';
 
 const Search = () => {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const query = searchParams.get('query') || '';
   const { data, fetchNextPage, isFetching, refetch } = useSearchThemeInfiniteQuery(query);
   const targetRef = useRef<HTMLDivElement>(null);
-
+  const lastVisited = localStorage.getItem('lastVisited');
   useEffect(() => {
     refetch();
   }, [query, refetch]);
@@ -25,7 +27,20 @@ const Search = () => {
     <Layout>
       {
         <>
-          <InfoText>{query === '' ? '전체 테마결과' : `'${query}'  검색 결과`}</InfoText>
+          <TopWrapper>
+            <InfoText>{query === '' ? '전체 테마결과' : `'${query}'  검색 결과`}</InfoText>
+            <Button
+              isIcon={false}
+              onClick={() => {
+                if (lastVisited !== null) {
+                  navigate(lastVisited);
+                }
+              }}
+            >
+              <>이전 페이지로 돌아가기</>
+            </Button>
+          </TopWrapper>
+
           {isFetching && <TextInfo>검색 중...</TextInfo>}
           {data?.pages.map((page) => (
             <>
@@ -50,7 +65,7 @@ const Search = () => {
 export default Search;
 
 const Layout = styled.div([
-  tw`w-full mx-auto mt-[10rem] pb-[4rem]`,
+  tw`w-full mx-auto mt-[4rem] pb-[4rem]`,
   tw`desktop:(max-w-[102.4rem])`,
   tw`tablet:(max-w-[78rem])`,
   tw`mobile:(max-w-[80%])`,
@@ -63,13 +78,19 @@ const Layout = styled.div([
   `,
 ]);
 
-const InfoText = styled.div([
-  tw`font-maplestory text-xl text-white self-start`,
-  tw`desktop:(top-[-4rem] left-[4rem] mb-6)`,
-  tw`tablet:(top-[-4rem] left-[15rem] mb-4)`,
-  tw`mobile:(top-[-4rem] left-[2rem])`,
+const TopWrapper = styled.div([
   css`
-    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  tw`w-full mb-2 px-2 desktop:(max-w-[97rem]) tablet:(max-w-[47.5rem]) mobile:(max-w-[34rem])`,
+]);
+
+const InfoText = styled.div([
+  tw`font-maplestory text-xl text-white`,
+  css`
+    flex: 1;
   `,
 ]);
 
