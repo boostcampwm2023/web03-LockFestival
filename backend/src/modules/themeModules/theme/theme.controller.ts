cfa2ab3b8ac3c5f731ea06d8364ef1104562c449
JEM1224 <jmeve24@naver.com>
2023-12-11 02:14:17 +0900

    ♻️ 테마 스웨거 정리

--- backend/src/modules/themeModules/theme/theme.controller.ts
+++ backend/src/modules/themeModules/theme/theme.controller.ts
@@ -7,7 +7,7 @@ import {
   Query,
   UseInterceptors,
 } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { ThemeService } from '@theme/theme.service';
 import { GenreThemesResponseDto } from '@theme/dtos/genre.themes.response.dto';
 import { ThemeResponseDto } from '@theme/dtos/theme.response.dto';
@@ -23,6 +23,16 @@ import { DateRequestDto } from '@theme/dtos/date.request.dto';
 import { CacheInterceptor, CacheTTL } from '@nestjs/cache-manager';
 import { TimeTableDto } from '@crawlerUtils/dtos/timetable.response.dto';
 import { MIN_TO_MILLI } from '@constants/time.converter';
+import {
+  GetGenreThemesSwagger,
+  GetGenresSwagger,
+  GetLocationThemesSwagger,
+  GetRandomGenresThemesSwagger,
+  GetSimpleThemesSwagger,
+  GetThemeDetailsSwagger,
+  GetThemesSwagger,
+  GetTimeTableSwagger,
+} from '@utils/swagger/theme.swagger.decorator';
 
 const DEFAULT_THEME_COUNT = 10;
 const TIMETABLE_TTL = MIN_TO_MILLI * 5;
@@ -36,15 +46,7 @@ export class ThemeController {
   ) {}
 
   @Get(':themeId/details')
-  @ApiOperation({
-    summary: '특정 테마의 상세 정보 반환',
-    description: '특정 테마의 상세 정보를 반환합니다.',
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: ThemeBranchThemesDetailsResponseDto,
-  })
+  @GetThemeDetailsSwagger()
   async getThemeDetails(
     @Param('themeId', ParseIntPipe) themeId: number
   ): Promise<ThemeBranchThemesDetailsResponseDto> {
@@ -52,27 +54,7 @@ export class ThemeController {
   }
 
   @Get('/random-genres')
-  @ApiOperation({
-    summary: '랜덤한 장르의 테마들을 반환합니다.',
-    description: '랜덤한 분류용 장르를 genreCount개 선택하여 각 themeCount개의 테마를 반환합니다.',
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: [GenreThemesResponseDto],
-  })
-  @ApiQuery({
-    name: 'genreCount',
-    required: false,
-    description: '랜덤한 장르의 개수(기본값: 3)',
-    type: Number,
-  })
-  @ApiQuery({
-    name: 'themeCount',
-    required: false,
-    description: '특정 장르의 랜덤한 테마의 개수(기본값: 10)',
-    type: Number,
-  })
+  @GetRandomGenresThemesSwagger()
   async getRandomGenresThemes(
     @Query('genreCount', new DefaultValuePipe(3), ParseIntPipe) genreCount: number,
     @Query('themeCount', new DefaultValuePipe(DEFAULT_THEME_COUNT), ParseIntPipe) themeCount: number
@@ -81,16 +63,7 @@ export class ThemeController {
   }
 
   @Get('/location')
-  @ApiOperation({
-    summary: '위치 기반 매장 테마 반환',
-    description:
-      '현재 위치에서 위도(x), 경도(y) 값을 받아 boundary KM 이내의 테마들을 커서 기반 페이지네이션으로 반환합니다.',
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: ThemeLocationResponseDto,
-  })
+  @GetLocationThemesSwagger()
   async getLocationThemes(
     @Query() themeLocationDto: ThemeLocationDto
   ): Promise<ThemeLocationResponseDto> {
@@ -98,25 +71,7 @@ export class ThemeController {
   }
 
   @Get('/genres/:genreId')
-  @ApiOperation({
-    summary: '특정 (분류용) 장르의 테마 리스트 반환',
-    description:
-      '특정 (분류용) 장르의 테마 리스트를 반환합니다. count 값이 주어지면 해당 개수만큼 반환합니다.',
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: [ThemeResponseDto],
-  })
-  @ApiParam({
-    description: '장르 id',
-    name: 'genreId',
-  })
-  @ApiQuery({
-    description: `가져올 테마 개수(기본값: ${DEFAULT_THEME_COUNT})`,
-    name: 'count',
-    required: false,
-  })
+  @GetGenreThemesSwagger()
   async getGenreThemes(
     @Param('genreId', ParseIntPipe) genreId: number,
     @Query('count', new DefaultValuePipe(DEFAULT_THEME_COUNT), ParseIntPipe) count: number
@@ -125,48 +80,19 @@ export class ThemeController {
   }
 
   @Get('/genres')
-  @ApiOperation({
-    summary: '분류 장르 리스트 반환',
-    description: '분류용 장르를 모두 반환합니다.',
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: [GenreDto],
-  })
+  @GetGenresSwagger()
   async getGenres(): Promise<GenreDto[]> {
     return await this.genreService.getAllGenres();
   }
 
   @Get('/simple-themes')
-  @ApiOperation({
-    summary: '검색 리스트 반환',
-    description: '모집글 생성 시 검색한 테마 리스트를 10개 반환합니다',
-  })
-  @ApiQuery({
-    description: '검색어',
-    name: 'query',
-    required: true,
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: [ThemeSimpleSearchResponseDto],
-  })
+  @GetSimpleThemesSwagger()
   async getSimpleThemes(@Query('query') query: string): Promise<ThemeSimpleSearchResponseDto[]> {
     return await this.themeService.getSimpleThemesBySearch(query);
   }
 
   @Get('/')
-  @ApiOperation({
-    summary: '검색에 따른 테마 리스트 반환',
-    description: '검색에 따른 테마리스트를 반환합니다.',
-  })
-  @ApiOkResponse({
-    status: 200,
-    description: '',
-    type: ThemeSearchResponseDto,
-  })
+  @GetThemesSwagger()
   async getThemes(
     @Query() themeSearchRequestDto: ThemeSearchRequestDto
   ): Promise<ThemeSearchResponseDto> {
@@ -174,15 +100,7 @@ export class ThemeController {
   }
 
   @Get('/:themeId/timetable')
-  @ApiOperation({
-    summary: '테마 시간표를 반환',
-    description: '입력받은 테마 id와 날짜로 시간표를 검색하여 반환합니다.',
-  })
-  @ApiQuery({
-    name: 'date',
-    type: Date,
-    example: new Date(),
-  })
+  @GetTimeTableSwagger()
   @UseInterceptors(CacheInterceptor)
   @CacheTTL(TIMETABLE_TTL)
   async getTimeTable(
