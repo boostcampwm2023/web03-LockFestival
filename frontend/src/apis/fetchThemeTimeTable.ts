97b5420a579125db94f1d5a4ca8000ecfb8de581
navyjeongs <wjdtjr8649@naver.com>
2023-12-12 17:16:59 +0900

    ✨ 필요없는 옵션 삭제 및 timeout 5초로 설정

--- frontend/src/apis/fetchThemeTimeTable.ts
+++ frontend/src/apis/fetchThemeTimeTable.ts
@@ -2,8 +2,13 @@ import userInstance from '@config/userInstance';
 import { Value } from 'react-calendar/dist/cjs/shared/types';
 
 const fetchThemeTimeTable = async (themeId: number, date: Value) => {
-  return (await userInstance({ method: 'get', url: `/themes/${themeId}/timetable?date=${date}` }))
-    .data;
+  return (
+    await userInstance({
+      method: 'get',
+      url: `/themes/${themeId}/timetable?date=${date}`,
+      timeout: 5000,
+    })
+  ).data;
 };
 
 export default fetchThemeTimeTable;
