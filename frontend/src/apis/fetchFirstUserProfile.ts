c315c7c60242fc8ef8cb47ece7b50f68ae251f0e
navyjeongs <wjdtjr8649@naver.com>
2023-12-12 16:23:46 +0900

    🏷️ profile api 타입 작성

--- frontend/src/apis/fetchFirstUserProfile.ts
+++ frontend/src/apis/fetchFirstUserProfile.ts
@@ -1,9 +1,10 @@
 import { SERVER_URL } from '@config/server';
 import axios from 'axios';
+import { Profile } from 'types/profile';
 
 const fetchFirstUserProfile = async (token: string) => {
   const res = (
-    await axios({
+    await axios<Profile>({
       method: 'get',
       url: SERVER_URL + '/users/profile',
       headers: { Authorization: `Bearer ${token}` },
