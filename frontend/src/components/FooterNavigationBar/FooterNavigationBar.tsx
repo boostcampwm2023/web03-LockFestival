182953a930de5c9e8b89b56821650b966071d2be
navyjeongs <wjdtjr8649@naver.com>
2024-01-06 16:00:23 +0900

    💄 FooterNavigationBar 디자인

---
+++ frontend/src/components/FooterNavigationBar/FooterNavigationBar.tsx
@@ -0,0 +1,90 @@
+import tw, { styled, css } from 'twin.macro';
+import { FaChildren } from 'react-icons/fa6';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { FaComment } from 'react-icons/fa6';
+import { FaHouse } from 'react-icons/fa6';
+import { NavMenu } from 'types/navMenu';
+
+const FooterNavigationBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSelectedItem = (item: NavMenu) => {
+    navigate(`${item}`);
+  };
+
+  return (
+    <FooterBar>
+      <Container>
+        <Content>
+          <IconWrapper isSelected={location.pathname === '/recruitment'}>
+            <FaChildren
+              size={28}
+              onClick={() => handleSelectedItem('/recruitment')}
+              color={location.pathname === '/recruitment' ? '#F2F2F2' : '#1A1A1A'}
+            />
+          </IconWrapper>
+        </Content>
+        <Content>
+          <IconWrapper isSelected={location.pathname === '/'}>
+            <FaHouse
+              size={28}
+              onClick={() => handleSelectedItem('/')}
+              color={location.pathname === '/' ? '#F2F2F2' : '#1A1A1A'}
+            />
+          </IconWrapper>
+        </Content>
+        <Content>
+          <IconWrapper isSelected={location.pathname === '/room-list'}>
+            <FaComment
+              size={28}
+              onClick={() => handleSelectedItem('/room-list')}
+              color={location.pathname === '/room-list' ? '#F2F2F2' : '#1A1A1A'}
+            />
+          </IconWrapper>
+        </Content>
+      </Container>
+    </FooterBar>
+  );
+};
+
+const FooterBar = styled.div(
+  [tw`hidden w-full bg-white mx-auto`, tw`mobile:(block)`],
+  css`
+    position: sticky;
+    bottom: 0;
+  `
+);
+
+const Container = styled.div([
+  css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+]);
+
+const Content = styled.div([
+  tw`w-[8rem] h-[4.8rem]`,
+  css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  `,
+]);
+
+const IconWrapper = styled.div<{ isSelected: boolean }>(({ isSelected }) => [
+  tw`h-[4rem] w-[4rem]`,
+  css`
+    border: 1px solid #1a1a1a;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: ${isSelected ? '#1A1A1A' : '#F2F2F2'};
+  `,
+]);
+
+export default FooterNavigationBar;
